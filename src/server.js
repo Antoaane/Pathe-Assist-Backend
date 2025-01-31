bff3ec2eeea1373a7434a40ee0876a186d7db8d8
Antoine Lauzis <antoinelauzis@gmail.com>
2025-01-31 20:26:52 +0100

    feat : listen edit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const { server } = require('./app'); // Importe le serveur HTTP partagé
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
